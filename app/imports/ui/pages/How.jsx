d731268e2cab40ec39da829ad53b12166ac5757c
jjgonzalez10 <jj.gonzalez10@uniandes.edu.co>
2018-10-28 22:05:48 -0500

    comments for how

--- app/imports/ui/pages/How.jsx
+++ app/imports/ui/pages/How.jsx
@@ -8,6 +8,9 @@ export default class Signout extends React.Component {
     return (
       <Header textAlign="center">
         <p>This is an application that helps users find a proper routin for the gym.</p>
+        <p>For the login you can enter as a Trainer or as a user.</p>
+        <p>If you enter as a Trainer you have a few options than users doesnt have.For example, you can add a routine or see all the routines</p>
+        <p>If you enter as a User you only can see the list of routines that the trainers had already pushed and you can rating all of them</p>       
         <p>If you like a trainers's routine, you can always contact him</p>
       </Header>
     );
